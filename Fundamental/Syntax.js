c2afb7031fe926fc1d0249b6d0b1aae4c24de99f
nhatphanhk <nhatphanhk102@gmail.com>
2026-01-31 17:01:51 +0700

    Add initial implementation of DataTypes, Syntax, and Variables files with foundational JavaScript concepts

---
+++ Fundamental/Syntax.js
@@ -0,0 +1,87 @@
+/*
+Whitespace
+JavaScript has the following whitespace: 
+*/
+
+//  - Carriage return. Example: \r
+//  - Space.
+//  - New Line
+//  - Tab
+
+/*
+Statements
+Statements is a piece of code that either declares a variable or intructs the JavaCripts engine to perform a task and ends with a semicolon(Example: (;))but this is optional.(Should use it to terminate a statement)
+ */
+
+/*
+Blocks
+A block is a sequence of zero or more statements. A block is delimeted by a pair of curly brackets (Example: {} ).
+*/
+
+/**
+ *Identifiers
+ is a name choose used to identify variables, functions, variables, parameters, etc.
+ Begin with a letter (A-Z or a-z), underscore (_), or dollar sign ($). Subsequent characters can also be digits (0-9).
+ Note: Identifiers in JavaScript are case-sensitive. Example: message is different from the Message
+ */
+
+/**
+  * Comments
+  Allow to include notes or hints in JavaScript code, the JavaScript engine ignores the comments
+    There are two types of comments in JavaScript"
+    - Single-line comments: start with // and extend to the end of the line
+    - Block comments: start with /* and end with *\/ (without the backslash)
+  */
+
+/**
+ * Expressions
+ is a piece of code that produces a value. Example: 5 + 2 is an expression that produces the value 7
+ */
+
+/**
+  * Keywords & Reserved Words
+  JavaScript defines a list of reserved keywords that have specific uses so cannot use the reserved keywords as identifiers
+    - break
+    - case
+    - catch
+    - continue
+    - debugger
+    - default
+    - else
+    - export
+    - extends
+    - function
+    - if
+    - import
+    - new
+    - return
+    - super
+    - throw
+    - try
+    - null
+    - void
+    - while
+    - with
+    - class
+    - delete
+    - finally
+    - in
+    - switch
+    - typeof
+    - yield
+    - const
+    - do
+    - for
+    - instanceof
+    - this
+    - var
+    - let
+    - enum
+    - implements
+    - protectd
+    - private
+    - public
+    - interface
+    - package
+    - await
+  */
---
+++ Fundamental/Variables.js
@@ -0,0 +1,13 @@
+/**
+ * Rule to get name for variable declarations.
+    - Name for variable declarations is case sensitive.
+    - Variable names must start with a letter, underscore (_), or dollar sign ($).
+    - Variable names can not be a reserved keyword in JavaScript.
+ * Var is variable declaration.
+ * Let is block-scoped variable declaration.And using to assign value to variable.
+ * Const is constant and block-scoped variable declaration.And using to assign constant value to variable.
+ * Can declate multiple variables in one statement by using comma(,).
+ * distinguish between undefined and undeclarded variables is that 
+    - undefined variables are declared but have not been assigned a value, 
+    - while undeclared variables have not been declared in the current scope.
+ */
