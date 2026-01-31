b5d80e8384fd112fc97fc4ac8392584b340016a7
nhatphanhk <nhatphanhk102@gmail.com>
2026-01-31 18:26:57 +0700

    Add initial implementation of DataTypes.js with descriptions of JavaScript data types

---
+++ Fundamental/DataTypes.js
@@ -0,0 +1,47 @@
+//JavaScript is a dynamically typed language, which means that a variable can hold values of different data types at different times.
+
+/**
+ * The udefined type
+ * - Only one value: undefined
+ * - when a variable is declared but not assigned a value, it is automatically assigned the value undefined.
+ */
+
+/**
+ * The null type
+ * - Only one value: null
+ * - Null equal t undefined
+ */
+
+/**
+ * The number type 
+ * - Represents both integer and floating-point numbers
+ * - Special numeric values: Infinity, -Infinity, NaN (Not-a-Number)
+ *  + Any operation with NaN results in NaN
+ *  + The NaN value is not equal to anything, including itself 
+ */
+
+/**
+ * The string type
+ * - Represents a sequence of characters
+ * - Strings are immutable (cannot be changed after they are created)
+ * 
+ */
+
+/**
+ * The boolean type
+ * - Represents two values: true and false
+ * - Commonly used in conditional statements and logical operations
+ */
+
+/**
+ * The bigint type
+ * - Represents integers with arbitrary precision
+ * - Created by appending 'n' to the end of an integer literal
+ */
+
+/**
+ * The object type
+ * - Represents collections of key-value pairs
+ * - Can store multiple values and complex data structures
+ * - To access properties, use dot notation or bracket notation
+ */
